5601fb6101b003e2c5be2bf90f7a4005c489910b
Khaliddxx <doodz@aucegypt.edu>
2021-06-10 02:46:29 +0200

    Finished Submission

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ProfilePhoto from './Component/Profile/ProfilePhoto';
+import FullName from './Component/Profile/FullName';
+import Address from './Component/Profile/Address';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <FullName></FullName>
+      <Address></Address>
+{/* Experimenting with inline styling */}
+      <p style={{color:'#26496E', textAlign:'center'}}><b>Hover Over Picture</b></p>
+      <ProfilePhoto></ProfilePhoto>
+    </React.Fragment>
   );
 }
 
